0cadc1ad034c10de515e6c318db6334ac79674af
Vierling, Marvin <marvinvierling@hotmail.de>
2017-09-21 19:16:46 +0200

    gitignore + delete modules/dist

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    loginName: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    token: string;
+}
--- src/routes/HeroRouter.ts
+++ src/routes/HeroRouter.ts
@@ -1,6 +1,11 @@
 import { Router, Request, Response, NextFunction } from 'express';
 const Heroes = require('../data.json');
 
+
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from '../models/User';
+
 export class HeroRouter {
     router: Router
 
@@ -16,7 +21,25 @@ export class HeroRouter {
      * GET all Heroes.
      */
     public getAll(req: Request, res: Response, next: NextFunction) {
-        res.send(Heroes);
+        //res.send(Heroes);
+        createConnection({
+            type: "mysql",
+            host: "localhost",
+            port: 3306,
+            username: "root",
+            password: "",
+            database: "todo",
+            entities: [
+                User
+            ],
+            autoSchemaSync: true,
+        }).then(async connection => {
+            //console.log(connection);
+            let users = await connection.manager.find(User);
+            res.status(200).send(users);
+            // Here you can start to work with your entities
+            connection.close();
+        }).catch(error => {console.log(error); res.send(error)});
     }
 
     /**
