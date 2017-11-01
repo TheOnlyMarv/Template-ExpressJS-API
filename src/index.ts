32d537b4eba8440ae5e9f5c6c44e14b0069d6fc2
Vierling, Marvin <marvinvierling@hotmail.de>
2017-11-01 13:26:22 +0100

    add  headers

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ createConnection().then(async connection => {
     app.use(bodyParser.json());
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token");
         next();
     });
 
